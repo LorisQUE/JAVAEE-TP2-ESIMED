fa33786039e7c8d562d0506386cdd4e29e01f704
Loris Quetglas <36083989+LorisQUE@users.noreply.github.com>
2021-11-22 21:56:16 +0100

    Add Register + Logout
    - Addition of register and logout
    - After register, it automatically login
    - Redirection after login

--- src/java/montp/web/controllers/RegisterView.java
+++ src/java/montp/web/controllers/RegisterView.java
@@ -1,21 +1,25 @@
 package montp.web.controllers;
 
+import montp.data.model.security.Group;
 import montp.data.model.security.User;
 import montp.services.UserService;
-import montp.tools.Logger;
 
 import javax.annotation.PostConstruct;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @ViewScoped
 @Named("register")
 public class RegisterView implements Serializable {
 
-    @Inject
-    private UserService userService;
+    @Inject private HttpServletRequest httpServletRequest;
+    @Inject private UserService userService;
     private User user;
 
     @PostConstruct
@@ -23,8 +27,21 @@ public class RegisterView implements Serializable {
         this.user = new User();
     }
 
-    public void register() {
+    public void register() throws Exception {
 
+        // TODO : Après inscription, autoconnect et redirect sur l'index
+        if(user != null) {
+            String email = user.getEmail();
+            String password = user.getPassword();
+
+            List<Group> groupes = new ArrayList<>();
+            groupes.add(userService.getGroup("USER"));
+            user.setGroups(groupes);
+            userService.insert(user);
+
+            httpServletRequest.login(email, password);
+            FacesContext.getCurrentInstance().getExternalContext().redirect("index.xhtml");
+        }
     }
 
     public User getUser() {
